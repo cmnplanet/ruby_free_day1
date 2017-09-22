9000b12ca9c69e338a985b32b72f7849c913085b
Christopher Nimz <cmnfv@mac.com>
2017-09-21 22:08:07 -0400

    three

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,19 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  # before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @book = Book.all
   end
 
   # GET /books/1
   # GET /books/1.json
   def show
+    @book = Book.find(params[:id])
   end
+
 
   # GET /books/new
   def new
@@ -19,45 +22,43 @@ class BooksController < ApplicationController
 
   # GET /books/1/edit
   def edit
+    @book = Book.find(params[:id])
   end
 
   # POST /books
   # POST /books.json
   def create
     @book = Book.new(book_params)
-
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    @book.user = current_user
+    if @book.save
+      redirect_to @book
+    else
+      render 'new'
     end
   end
 
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
-    respond_to do |format|
+    @book = Book.find(params[:id])
+    if @book.user == current_user
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
+        redirect_to @book
       else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+        render 'edit'
       end
+    else
+      redirect_to root
     end
   end
 
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
-    @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully burned.' }
-      format.json { head :no_content }
+    if @book.destroy
+      redirect_to books_path
+    else
+      redirect_to root
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  get '/' => 'books#index'
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
